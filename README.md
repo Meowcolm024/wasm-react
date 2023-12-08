052f2e71ace277305bcc310d4d5b9b41ba9ffc15
Meowcolm024 <luoyicong1026@live.com>
2023-12-08 12:32:30 +0800

    update

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # wasm + react
 
 A **brainfuck** interpreter using rust wasm + webpack + react.
+
+build project: `npm run build`
+
+run local server: `npm run serve`
--- index.js
+++ index.js
@@ -1,8 +1,1 @@
 import './public/index.css'
-// import { greet, run_bf } from './pkg';
-
-// let input = "++++++++++[>+++++++>++++++++++>+++>+<<<<-]>++.>+.+++++++..+++.>++.<<+++++++++++++++.>.+++.------.--------.>+.>."
-// let res = run_bf(input)
-
-// greet('World');
-// alert(res);
